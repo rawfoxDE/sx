ff4e647c945faaf9118419dd2bd2895475c203e5
Michael Clasen <37894267+rawfoxDE@users.noreply.github.com>
2024-05-22 20:09:04 +0200

    Add files via upload

---
+++ include/fifopr.h
@@ -0,0 +1,7 @@
+/* "fifo.library"*/
+#pragma libcall FifoBase OpenFifo 1E 81003
+#pragma libcall FifoBase CloseFifo 24 1002
+#pragma libcall FifoBase ReadFifo 2A 81003
+#pragma libcall FifoBase WriteFifo 30 81003
+#pragma libcall FifoBase RequestFifo 36 81003
+#pragma libcall FifoBase BufSizeFifo 3C 001
